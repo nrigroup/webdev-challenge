832b3dc7c0478aab701acfc6920710acf401b1a4
Daniel Petersen <dpetersen1@live.com>
2018-01-08 15:49:49 -0800

    file logic uncommented

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -19,27 +19,27 @@ class DataController extends Controller
     	$file = $request->file('csv');
     	$row = 0;
 
-		// if (($handle = fopen($file, "r")) !== FALSE) {
-		//     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-		//     	// skip header row
-		//         if ($row > 0){			        
-		//         	$item = new Item;
+		if (($handle = fopen($file, "r")) !== FALSE) {
+		    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+		    	// skip header row
+		        if ($row > 0){			        
+		        	$item = new Item;
 
-  //   				$item->date = $data[0];
-		//             $item->category = $data[1];
-		//             $item->lot_title = $data[2];
-		//             $item->lot_location = $data[3];
-		//             $item->lot_condition = $data[4];
-		//             $item->pre_tax_amount = $data[5];
-		//             $item->tax_name = $data[6];
-		//             $item->tax_amount = $data[7];
+    				$item->date = $data[0];
+		            $item->category = $data[1];
+		            $item->lot_title = $data[2];
+		            $item->lot_location = $data[3];
+		            $item->lot_condition = $data[4];
+		            $item->pre_tax_amount = $data[5];
+		            $item->tax_name = $data[6];
+		            $item->tax_amount = $data[7];
 
-		//             $item->save();
-		// 	    }
-		// 	    $row++;
-		//     }
-		//     fclose($handle);
-		// }
+		            $item->save();
+			    }
+			    $row++;
+		    }
+		    fclose($handle);
+		}
 
 		$totals = Item::totals();
 
