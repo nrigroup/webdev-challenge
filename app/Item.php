581fd169b3e9e391b028e448293c549f43bfca2d
Daniel Petersen <dpetersen1@live.com>
2018-01-08 15:43:56 -0800

    total spending table added

--- app/Item.php
+++ app/Item.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Item extends Model
 {
-    //
+    protected static function totals(){
+    	return Item::select('category', DB::raw('SUM(pre_tax_amount) as total'))
+                ->groupBy('category')
+                ->get();
+    }
 }
--- resources/views/upload/done.blade.php
+++ resources/views/upload/done.blade.php
@@ -3,12 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading">Upload</div>
+                <div class="panel-heading">Item Totals</div>
 
                 <div class="panel-body">
-                    <h1>Thank you</h1>
+                    <h1>Item totals</h1>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Category</th>
+                                <th>Total spending</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($totals as $total)
+                            <tr>
+                                <th>{{ $total->category }}</th>
+                                <td>${{ $total->total }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>                        
+                    </table>
                 </div>
             </div>
         </div>
