a09a1ad59dc4c25bf5072526e3d867f971eb3187
Craig Garrett <clgarrett73@gmail.com>
2018-01-20 14:06:10 -0500

    separate the Validation logic from the controllers and models

---
+++ app/Http/Requests/LotRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LotRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'item_file' => 'required|max:10000|mimes:csv,txt'
+        ];
+    }
+}
