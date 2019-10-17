6609bd7754fb1e9e021555e57258398d45d86a70
D.Cherouati <ch.djillali@gmail.com>
2019-10-17 15:24:59 -0500

    Cleaning up / refaktoring - navigation bar in the layout file and not every view

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,4 +1,3 @@
-<!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -13,9 +12,41 @@
   </head>
   <body>
     <div class="text-center">
-	    <div class="container">
-	        NRI Industrail
-	    </div> 
+      <div class="container">
+        <h1>NRI Industrail</h1>
+      </div>                
+
+      <div class="text-center">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/upload/">Upload a file</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/report/">Generate report</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/categories/">Manage categories</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/conditions/">Manage conditions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/lots/">Manage lots</a>
+              </li>              
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
   		@yield ('content')
 
     <!-- Optional JavaScript -->
--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,39 +1,5 @@
 @extends ('layout')
 @section ('content')
-<div class="container">
-    <h1>Report page: </h1>
-</div>                
-
-    <div class="text-center">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="/upload/">Upload a file</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/categories/">Manage categories</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/conditions/">Manage conditions</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/lots/">Manage lots</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://github.com/laravel/laravel">GitHub</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-    </div>
 
     <table class="table table-dark">
   	  <tbody>
