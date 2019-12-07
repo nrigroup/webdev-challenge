61f0f6d78c14924a064392560d039d613ba71fb6
Hiren bhatiya <44989209+hirenbhatia007@users.noreply.github.com>
2019-12-06 23:44:53 -0500

    challenge submission

---
+++ laravel/app/Item.php
@@ -0,0 +1,8 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    public $fillable = ['date', 'category', 'lot_title', 'lot_location', 'lot_condition', 'pre_tax_amount', 'tax_name', 'tax_amount'];
+}
