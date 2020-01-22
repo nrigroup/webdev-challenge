6a36e1ae1e2954a0e1649ab1dc878303b7cab407
Negin Mashreghi <negin_mashreghi@yahoo.com>
2020-01-22 14:32:32 -0500

    create auction Controller

--- app/Auction.php
+++ app/Auction.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Auction extends Model
 {
-    //
+    protected $fillable = [
+        'date', 'category', 'lot_title', 'lot_location', 'lot_condition' ,'pretax_amount', 'tax_name', 'tax_amount'
+    ];
 }
---
+++ app/Http/Controllers/AuctionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuctionController extends Controller
+{
+    
+    // store CEV file into database
+    public function store(Request $request){
+
+    }
+
+
+
+}
