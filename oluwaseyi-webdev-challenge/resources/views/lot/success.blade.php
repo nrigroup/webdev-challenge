d73ac9cbaa6b5c7e23a15e990613f5a051e3a5dd
Oluwaseyi Olarotimi <olarotseyi@gmail.com>
2020-01-23 20:13:43 -0500

    Added dollar sign to prices

--- oluwaseyi-webdev-challenge/resources/views/lot/success.blade.php
+++ oluwaseyi-webdev-challenge/resources/views/lot/success.blade.php
@@ -7,7 +7,7 @@
 <h3>Spending By Category</h3>
 <ul>
 @foreach($spendingByCategory as $spending)
-    <li>{{ $spending->category }} - {{ $spending->total_spending }}</li>
+    <li>{{ $spending->category }} - ${{ $spending->total_spending }}</li>
 @endforeach
 </ul>
 
@@ -15,7 +15,7 @@
 
 <ul>
 @foreach($spendingPerMonth as $spending)
-    <li>{{ $spending->month }}/{{ $spending->year }} - {{ $spending->total_spending }}</li>
+    <li>{{ $spending->month }}/{{ $spending->year }} - ${{ $spending->total_spending }}</li>
 @endforeach
 </ul>
 
