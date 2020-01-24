c69eca097159068fb83cf11f1fa717d561038e07
redrect <chenmrkent@hotmail.com>
2020-01-24 14:19:07 -0500

    Read the upload csv file and write into database
    - reformat the date to the mysql default date format before write in

--- webdev-challenge/app/Http/Controllers/FileserverController.php
+++ webdev-challenge/app/Http/Controllers/FileserverController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Data;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
@@ -9,12 +10,16 @@ class FileserverController extends Controller
 {
     public function storeFile (Request $request)
     {
-        if ($request->hasFile('fileinput') && $request->file('fileinput')->isValid()) {
-                $filename = uniqid().'_'.$request->file('fileinput')->getClientOriginalName();
+        $file = $request->file('fileinput');
+        if ($request->hasFile('fileinput') && $file->isValid()) {
 
-                $path = $request->file('fileinput')->storeAs(
-                    'public/storage', $filename
-                );
+            $filename = uniqid().'_'.$file->getClientOriginalName();
+            $path = storage_path('app/public/storage/');
+            $request->file('fileinput')->storeAs(
+                'public/storage/', $filename
+            );
+            $this->readFile($file,$path.$filename);
+
             return [
                 'msg'       => 'ok',
                 'status'    => '200',
@@ -28,8 +33,53 @@ class FileserverController extends Controller
             ];
         }
 
+    }
 
+    public function readFile($file, $path){
 
+        $file = fopen($path, "r");
 
+        $importData_arr = array();
+        $i = 0;
+
+        while (($filedata = fgetcsv($file, 1000, ",")) !== FALSE) {
+            $num = count($filedata );
+
+            // Skip first row
+            if($i == 0){
+               $i++;
+               continue;
+            }
+
+            for ($c=0; $c < $num; $c++) {
+                if($c == 0)
+                {
+                     $date = date_create($filedata [$c]);
+                     $date = date_format($date,'Y-m-d');
+                    $importData_arr[$i][] = $date;
+                }
+                else
+                    $importData_arr[$i][] = $filedata [$c];
+            }
+
+            $i++;
+        }
+        fclose($file);
+
+        //insert into db
+        foreach($importData_arr as $importData){
+
+            $insertData = array(
+                "date"=>$importData[0],
+                "category"=>$importData[1],
+                "lot title"=>$importData[2],
+                "lot location"=>$importData[3],
+                "lot condition"=>$importData[4],
+                "pre-tax amount"=>$importData[5],
+                "tax name"=>$importData[6],
+                "tax amount"=>$importData[7]);
+            Data::insertData($insertData);
+
+        }
     }
 }
