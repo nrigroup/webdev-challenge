3196e8f186247c7a98707eb64ac438f3d1d673e4
Tez01 <tejpartap02@gmail.com>
2022-12-02 17:03:59 -0500

    Add validation for checking file type to be only csv

--- frontend_react/frontend/src/pages/Home.js
+++ frontend_react/frontend/src/pages/Home.js
@@ -1,12 +1,38 @@
+import { useState } from 'react';
+
 function Home() {
+    const [error, setError] = useState('');
+    const handleFileUpload = (e) => {
+        e.preventDefault();
+
+        setError('');
+        // Check if file is csv or not
+        if (!e.target.files[0].name.match(/\.(csv)$/i)) {
+            console.log('Not a csv file');
+            setError('Not a csv file');
+        }
+    };
     return (
-        <section className="bg-light text-dark p-5 text-center">
-            <input
-                className="form-control mx-auto form-file"
-                type="file"
-                id="formFile"
-            />
-        </section>
+        <div className="bg-light text-dark p-5 text-center">
+            <form>
+                <input
+                    className="form-control mx-auto form-fileInput "
+                    type="file"
+                    onChange={handleFileUpload}
+                />
+                <button type="submit" className="btn btn-primary my-3">
+                    Upload
+                </button>
+            </form>
+
+            {error ? (
+                <div className="alert alert-primary" role="alert">
+                    {error}
+                </div>
+            ) : (
+                ''
+            )}
+        </div>
     );
 }
 
