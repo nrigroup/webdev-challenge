b58e017fff9fd3dc6cc180fc88873264f4d7a769
Tez01 <tejpartap02@gmail.com>
2022-12-04 12:45:42 -0500

    Add rule violation checks on file, Add relevant tests

--- frontend_react/frontend/src/pages/Home.js
+++ frontend_react/frontend/src/pages/Home.js
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 
-function checkFileFormat(file, pattern) {
+const REQUIRED_NUM_OF_HEADERS = 8;
+const REQUIRED_HEADERS = [
+    'date',
+    'category',
+    'lot title',
+    'lot location',
+    'lot condition',
+    'pre-tax amount',
+];
+export function checkFileExtension(filename, pattern) {
     let validity = true;
     // Check file format
-    if (!file.name.match(pattern)) {
+    if (!filename.match(pattern)) {
         validity = false;
         return validity;
     }
@@ -11,31 +20,145 @@ function checkFileFormat(file, pattern) {
     return validity;
 }
 
-function checkDataValidity(file) {}
+// Returns a string of data in the file object. Empty string if any error
+export async function fileToString(file) {
+    if (!file) {
+        return '';
+    }
+
+    const str = await file.text();
+
+    if (str === undefined) {
+        return '';
+    }
+
+    return str;
+}
+
+// Converts data content of csv file as string to an array. Returns an
+// array of objects where each object represents a row.
+// if successful an array is returned, otherwise undefined is returned
+function csvStringToArray(str, delimiter = ',') {
+    if (str.length === 0) {
+        return undefined;
+    }
+
+    let arr;
+    // slice from start of text to the first \n index
+    // use split to create an array from string by delimiter
+    const headers = str.slice(0, str.indexOf('\n')).split(delimiter);
+    if (headers === '') {
+        return undefined;
+    }
+
+    // slice from \n index + 1 to the end of the text
+    // use split to create an array of each csv value row
+    const rows = str.slice(str.indexOf('\n') + 1).split('\n');
+    if (rows === '') {
+        return undefined;
+    }
+
+    return arr;
+}
+
+// Returns true if element exist in array
+export function findElementInArray(element, arr) {
+    for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
+// Returns an error message string if data violates the rules for csv files,
+// Returns empty string, if all rules followed
+export function findRuleViolation(csvString) {
+    // Array of strings, where each string is a header name
+    let csvHeaders = csvString.slice(0, csvString.indexOf('\n')).split(',');
+
+    // Data is invalid, if minimum headers are not present
+    if (csvHeaders.length < REQUIRED_NUM_OF_HEADERS) {
+        return 'Minimum headers not present';
+    }
+
+    // if csv headers dont have a name, give them names explicitly
+    if (csvHeaders[0] === '') {
+        csvHeaders = [
+            'date',
+            'category',
+            'lot title',
+            'lot location',
+            'lot condition',
+            'pre-tax amount',
+            'tax name',
+            'tax amount',
+        ];
+    }
+
+    // Array of strings, where each string represent a row
+    let csvRows = csvString.slice(csvString.indexOf('\n') + 1).split('\n');
+    csvRows = csvRows.slice(0, -1); // Remove last row as it is empty always due to \n
+    // Data is invalid, if there are no rows
+    if (csvRows.length < 1) {
+        return 'Empty Rows';
+    }
+
+    // Map each row to csv headers
+    for (let i = 0; i < csvRows.length; i += 1) {
+        const entries = csvRows[i].split(',');
+        for (let j = 0; j < entries.length; j += 1) {
+            const entry = entries[j];
+            const correspondingHeader = csvHeaders[j];
+            // Data is invalid if entry does not exist for required header
+            if (
+                entry === '' &&
+                findElementInArray(correspondingHeader, REQUIRED_HEADERS)
+            ) {
+                return 'Some required fields are missing';
+            }
+        }
+    }
+
+    return '';
+}
 function Home() {
+    // State for maintaining error
+    const [error, setError] = useState('');
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
         if (!e.target.checkValidity()) {
             e.stopPropagation();
         }
-        e.target.classList.add('was-validated');
     };
-    const handleFileValidity = (e) => {
-        // Get div for error display
-        const error = e.target.nextElementSibling;
-        // Check if file is csv or not
-        if (!checkFileFormat(e.target.files[0], /\.(csv)$/i)) {
+    const handleFileValidity = async (e) => {
+        // Add was validated class to parent form,so that UI starts displaying feedback
+        e.target.parentElement.classList.add('was-validated');
+
+        // Check if file extension is csv or not
+        if (!checkFileExtension(e.target.files[0].name, /\.(csv)$/i)) {
             // Set error message inside textContent of error
-            error.textContent = 'Not a cssv file';
+            setError('Not a csvs file');
+            // Set validity of this element to false so that form becomes invalid
+            e.target.setCustomValidity('Error');
+
+            return;
+        }
+
+        const csvString = await fileToString(e.target.files[0]);
+        if (csvString === '') {
+            // Set error message inside textContent of error
+            setError('Empty file');
             // Set validity of this element to false so that form becomes invalid
             e.target.setCustomValidity('Error');
 
             return;
         }
         // Check if data is valid or not
-        if (!checkDataValidity(e.target.files[0])) {
-            error.textContent = 'Data format is incorrect';
+        const ruleError = findRuleViolation(csvString);
+        if (!(ruleError === '')) {
+            // If there is an error in CSV data
+            setError(ruleError);
             // Set validity of this element to false so that form becomes invalid
             e.target.setCustomValidity('Error');
             return;
@@ -57,7 +180,7 @@ function Home() {
                     onChange={handleFileValidity}
                     required
                 />
-                <div className="invalid-feedback" />
+                <div className="invalid-feedback">{error}</div>
 
                 <button type="submit" className="btn btn-primary my-3">
                     Upload
--- frontend_react/frontend/src/pages/Home.test.js
+++ frontend_react/frontend/src/pages/Home.test.js
@@ -1,5 +1,56 @@
-import { findElementInArray } from './Home';
+import {
+    findElementInArray,
+    checkFileExtension,
+    findRuleViolation,
+} from './Home';
 
 test('Returns true if an element exist in array', () => {
     expect(findElementInArray(1, [1, 2, 3])).toBe(true);
+});
+
+test('Returns false if an element does not exist in array', () => {
+    expect(findElementInArray(0, [1, 2, 3])).toBe(false);
+});
+
+test('Returns true if filename matches pattern for csv extension', () => {
+    expect(checkFileExtension('data.csv', '/.(csv)$/i')).toBe(false);
+});
+
+test('Returns header error if required number of headers are not there', () => {
+    expect(
+        findRuleViolation(
+            'date,category,lot title,lot location,lot condition,pre-tax amount,tax name\n'
+        )
+    ).toBe('Minimum headers not present');
+});
+
+test('Returns empty rows error if there are no rows', () => {
+    expect(
+        findRuleViolation(
+            'date,category,lot title,lot location,lot condition,pre-tax amount,tax name,tax amount\n'
+        )
+    ).toBe('Empty Rows');
+});
+
+test('Returns required field error if required fields are missing', () => {
+    expect(
+        findRuleViolation(
+            'date,category,lot title,lot location,lot condition,pre-tax amount,tax name,tax amount\n12/1/2013,,Hauling Transfer Trailers,"783 Park Ave, New York, NY 10021",Brand New,350,NY Sales tax,31.06\n'
+        )
+    ).toBe('Some required fields are missing');
+});
+test('Returns empty string if all rules are followed in the csv data', () => {
+    expect(
+        findRuleViolation(
+            'date,category,lot title,lot location,lot condition,pre-tax amount,tax name,tax amount\n12/1/2013,Construction,Hauling Transfer Trailers,"783 Park Ave, New York, NY 10021",Brand New,350,NY Sales tax,31.06\n'
+        )
+    ).toBe('');
+});
+
+test('Returns empty string if all only non required fields are absent', () => {
+    expect(
+        findRuleViolation(
+            'date,category,lot title,lot location,lot condition,pre-tax amount,tax name,tax amount\n12/1/2013,Construction,Hauling Transfer Trailers,"783 Park Ave, New York, NY 10021",Brand New,350,NY Sales tax,\n'
+        )
+    ).toBe('');
 });
