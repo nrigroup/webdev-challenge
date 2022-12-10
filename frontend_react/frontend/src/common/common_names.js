e4ba2a9d104be5451643d16bcba2b3de990ec952
Tez01 <tejpartap02@gmail.com>
2022-12-09 20:09:40 -0500

    Add requirements.txt

---
+++ frontend_react/frontend/src/common/common_names.js
@@ -0,0 +1,32 @@
+export const TAX_FIELD_NAME = 'pre-tax amount';
+export const DATE_FIELD_NAME = 'date';
+export const CATEGORY_FIELD_NAME = 'category';
+export const LOT_CONDITION_FIELD_NAME = 'lot condition';
+export const SUCCESS_CODE = 200;
+
+export const REQUIRED_HEADERS = [
+    'date',
+    'category',
+    'lot title',
+    'lot location',
+    'lot condition',
+    'pre-tax amount',
+];
+
+export const ALL_HEADERS = [
+    'date',
+    'category',
+    'lot title',
+    'lot location',
+    'lot condition',
+    'pre-tax amount',
+    'tax name',
+    'tax amount',
+];
+
+export const REQUIRED_NUM_OF_HEADERS = 8;
+
+export const CSV_SPLIT_PATTERN = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
+export const BACKEND_API_URL = 'http://localhost:8000/api/';
+export const FAIL_RESPONSE_CODE = 400;
+export const SUCCESS_RESPONSE_CODE = 200;
